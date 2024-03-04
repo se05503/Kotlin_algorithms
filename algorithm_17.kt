121b00b48e42a15bc6e203208f13874bd3572e1a
SEYOUNG PARK <seyeongb18@gmail.com>
2024-03-04 22:57:36 +0900

    자연수 뒤집어 배열로 만들기

--- algorithm_17.kt
+++ algorithm_17.kt
@@ -1,1 +1,19 @@
-//commit
+class Solution {
+    fun solution(n: Long): IntArray {
+        var answer = intArrayOf()
+        var num : Long = n
+        while(num!=0L) {   
+            answer = answer + (num%10).toInt()
+            num /= 10
+        }
+        return answer
+    }
+}
+
+/*
+class Solution {
+    fun solution(n: Long): IntArray {
+        return n.toString().reversed().map { it.toString().toInt() }.toIntArray()
+    }
+}
+*/
