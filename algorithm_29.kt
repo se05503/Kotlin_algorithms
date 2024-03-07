20da982a691926b5c36aeba14c98ca4495bc9331
SEYOUNG PARK <seyeongb18@gmail.com>
2024-03-07 11:27:05 +0900

    제일 작은 수 제거하기

---
+++ algorithm_29.kt
@@ -0,0 +1,8 @@
+class Solution {
+    fun solution(arr: IntArray): IntArray {
+    var answer = intArrayOf()
+    answer = arr.filter{it!=arr.minOrNull()}.toIntArray()  
+    if(answer.size==0) answer += -1
+    return answer
+}
+}
