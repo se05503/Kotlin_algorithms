e6052c3fd3d1a426f0a9ef55ac9bc6482f4b5901
SEYOUNG PARK <seyeongb18@gmail.com>
2024-03-14 10:48:40 +0900

    행렬의 덧셈(정리X)

--- algorithm_37.kt
+++ algorithm_37.kt
@@ -1,5 +1,14 @@
+// 아직 티스토리 정리안함
+// 다른 사람 풀이 아직 참고안함
+
+fun main() {
+    var matrix1 = arrayOf(intArrayOf(1, 2, 3, 4), intArrayOf(2, 3, 2, 3))
+    var matrix2 = arrayOf(intArrayOf(3, 4, 5, 6), intArrayOf(5, 6, 1, 2))
+    print(solution(matrix1, matrix2))
+}
+
 fun solution(arr1: Array<IntArray>, arr2: Array<IntArray>): Array<IntArray> {
-    var answer = Array<IntArray>(arr1.size, { IntArray(arr1[0].size, { 0 }) }) // 답을 찾았다!!
+    var answer = Array<IntArray>(arr1.size, { IntArray(arr1[0].size, { 0 }) }) // 가장 중요한 코드
     if (arr1.size in 1..500 && arr1[0].size in 1..500 && arr2.size in 1..500 && arr2[0].size in 1..500) {
         for (i in 0 until arr1.size) {
             for (j in 0 until arr1[i].size) {
@@ -7,6 +16,6 @@ fun solution(arr1: Array<IntArray>, arr2: Array<IntArray>): Array<IntArray> {
             }
         }
     } else throw IllegalArgumentException("arr1과 arr2 의 행과 열의 사이즈는 1 이상 500 이하입니다!")
-    
+
     return answer
 }
